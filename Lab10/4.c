2d588005e79465c5b219da49f486e143e2017653
Jonny-UFRJ <joaoluisguio@gmail.com>
2015-12-17 10:06:44 -0200

    little more

--- Lab10/4.c
+++ Lab10/4.c
@@ -6,6 +6,7 @@
 #define NTHREADS 4
 
 sem_t fsem;
+int in, out;
 
 typedef struct queue {
     int val;
@@ -22,6 +23,7 @@ void printfBuffer() {
 }
 
 void* producer(void *args){
+    sem_wait(fsem);
 
 }
 
@@ -30,5 +32,30 @@ void* consumer(void *args){
 }
 
 int main(int argc, char *argv[]) {
+    pthread_t threads[NTHREADS];
+    int *id, t;
 
+    in = 0;
+    out = 0;
+
+    sem_init(&fcond, 0, 1);
+    sem_init(&scond, 0, 0);
+
+    for (t = 0; t < NTHREADS; t++) {
+        if ((id = (int *) malloc(sizeof(int))) == NULL) {
+            pthread_exit(NULL);
+            return 1;
+        }
+        if (t == 0)
+            pthread_create(&threads[t], NULL, t1, (void *) id);
+        if (t == 1)
+            pthread_create(&threads[t], NULL, t2, (void *) id);
+        if (t == 2)
+            pthread_create(&threads[t], NULL, t3, (void *) id);
+        if (t == 3)
+            pthread_create(&threads[t], NULL, t4, (void *) id);
+    }
+
+    pthread_exit(NULL);
+    return 0;
 }
