c6d4ba1917810d50bb1f0f7e996e023038788c6d
AndrewKaninchen <andre.luiz.coelho@outlook.com>
2016-01-06 02:39:57 -0200

    Still not working, so ilegível, wow
    não sério, se eu dormir eu não vou conseguir acordar e entender esta bosta

--- Trabalho01/concorrente_coelho.c
+++ Trabalho01/concorrente_coelho.c
@@ -17,26 +17,36 @@ double (*funcList[3])(double) = {func1, func2, func3};
 
 typedef struct Interval
 {
-	double (*func)(double);
-	double a;
-	double b;
-	double err;
+    double (*func)(double);
+    double a;
+    double b;
+    double err;
 }Interval;
 
-int nThreads;				//Número de Threads
-int *freeThreads; 			//Threads disponíveis
-Interval *threadIntervals; 	//Intervalo que cada thread tem que calcular
+typedef struct IntervalList
+{
+    Interval *intervals;
+    int size;
+    int maxSize;
+}IntervalList;
 
-int *goodToGo;				//Verifica se as threads têm intervalos a ser calculados
-char end;					//Verifica o término do programa
+int nThreads;                   //Número de Threads
+int *freeThreads;               //Threads disponíveis
+int nFreeThreads;               //Número de threads disponíveis
+int overloadMagicNumber = 10;   //Número mágico que define se uma thread está ou não sobrecarregada.
+IntervalList *threadIntervals;  //Vetor de Listas de Intervalos que cada thread tem para calcular
+IntervalList overloadIntervals; //Lista que contém Intervalos a ser calculados. Intervalos vão parar aqui quando threads ficam sobrecarregadas.
 
-pthread_mutex_t freeThreads_mutex, sum_mutex;
+int *goodToGo;              //Verifica se as threads têm intervalos a ser calculados
+char end;                   //Verifica o término do programa
+
+pthread_mutex_t freeThreads_mutex, overload_mutex;
 pthread_cond_t freeThreads_cond;
 
-double *finalSum;			//Vetor com resultado da aproximação. Cada posição representa a parcial encontrada por cada thread, para evitar sincronizações desnecessárias.
+double *finalSum;           //Vetor com resultado da aproximação. Cada posição representa a parcial encontrada por cada thread, para evitar sincronizações desnecessárias.
 
 //Pede uma thread para calcular o intervalo dado
-void askForThread(Interval interval);
+int askForThread(Interval interval);
 
 //Avisa para o programa que uma thread está livre
 void imFree(int id);
@@ -48,49 +58,83 @@ void* threadFunction(void* arg);
 void adaptativeQuadrature(Interval interval, int id);
 
 
-void askForThread(Interval interval)
+int askForThread(Interval interval)
 {
-	int i, j;
-	for (i = 0; i < nThreads; ++i)
-	{
-		pthread_mutex_lock(&freeThreads_mutex);
-		while(freeThreads[0]==-1)
-			pthread_cond_wait(&freeThreads_cond, &freeThreads_mutex);
+    int j;
+    pthread_mutex_lock(&freeThreads_mutex);
+    if(nFreeThreads != 0)
+    {
+        int id = freeThreads[0];
+        nFreeThreads--;
+        printf("Asking for Thread\n");
+        //jeito horrível plim plim (lerdo as fudge)
+        threadIntervals[id].intervals[0] = interval;
+        threadIntervals[id].size++;
+        printf("Thread %d recebendo trabalho...\n", id);
+        goodToGo[id] = 1;
+        for (j = 0; j < nThreads-1; ++j)
+            freeThreads[j] = freeThreads[j+1];            
+        freeThreads[nThreads-1] = -1;
+        pthread_mutex_unlock(&freeThreads_mutex);
+        return 1;
+    }
+    else
+    {
+        pthread_mutex_unlock(&freeThreads_mutex);
+        return 0;
+    }
+}
 
-		//jeito horrível plim plim (lerdo as fudge)
-		threadIntervals[freeThreads[0]] = interval;
-		goodToGo[freeThreads[0]] = 1;
-		for (j = 0; j < nThreads-1; ++j)
-			freeThreads[j] = freeThreads[j+1];
-		freeThreads[nThreads-1] = -1;
+void storeOverloadedIntervals(IntervalList list)
+{
+    pthread_mutex_lock(&freeThreads_mutex);
+    for (int i = list.size/2; i < list.size; ++i)
+    {
+        if(overloadIntervals.size + 1 >= overloadIntervals.maxSize)
+            overloadIntervals.intervals = (Interval *)realloc(overloadIntervals.intervals, (sizeof(Interval) * 2 * overloadIntervals.maxSize));
+        overloadIntervals.intervals[overloadIntervals.size] = list.intervals[i];
+        overloadIntervals.size++;
+        list.size--;
+    }
 
-		pthread_mutex_unlock(&freeThreads_mutex);
-	}
+    pthread_mutex_unlock(&freeThreads_mutex);
 }
 
 void imFree(int id)
 {
-	int i;
-	for (i = 0; freeThreads[i] != -1; ++i);
-	freeThreads[i] = id;
-	pthread_cond_signal(&freeThreads_cond);
+    int i;
+    pthread_mutex_lock(&freeThreads_mutex);
+    nFreeThreads++;
+    for (i = 0; freeThreads[i] != -1; ++i);
+    freeThreads[i] = id;    
+    printf("freeThreads[%d] = %d\n", i, id);
+    pthread_cond_signal(&freeThreads_cond);
+    pthread_mutex_unlock(&freeThreads_mutex);    
 }
 
 void* threadFunction(void* arg)
 {
-	int myid = *(int*)(arg);
-	while(!end)
-	{
-		printf("penes %d\n", myid);
-		while(!goodToGo[myid])
-			if(end)
-				pthread_exit(NULL);
-		adaptativeQuadrature(threadIntervals[myid], myid);
-	}
-	pthread_exit(NULL);
+    int myid = *(int*)(arg);
+    while(!end)
+    {
+        //printf("penes %d\n", myid);
+        while(!goodToGo[myid])
+            if(end)
+                pthread_exit(NULL);
+        while(threadIntervals[myid].size)
+        {
+            adaptativeQuadrature(threadIntervals[myid].intervals[0], myid);
+            for (int i = 1; i < threadIntervals[myid].size-1; ++i)
+            {
+                threadIntervals[myid].intervals[i-1] = threadIntervals[myid].intervals[i];
+            }
+            threadIntervals[myid].size--;
+        }
+    }
+    pthread_exit(NULL);
 }
 
-void adaptativeQuadrature(Interval interval, int id) 
+void adaptativeQuadrature(Interval interval, int id)
 {
     double m, funcB, funcSleft, funcSright, areaS1, areaS2, areaB;
     Interval leftInterval, rightInterval;
@@ -112,28 +156,35 @@ void adaptativeQuadrature(Interval interval, int id)
     areaB = (interval.b - interval.a) * funcB;
     areaS1 = (m - interval.a) * funcSleft;
     areaS2 = (interval.b - m) * funcSright;
+
+    printf("calculated\n");
     // agora, se o módulo da diferença entre a area do retangulo maior e da soma dos retangulos menores for maior que o erro máximo, calcula a área dos dois intervalos [a,m], [b,m]
     // senão, apenas retorna o valor da area do retangulo maior
     if (fabs(areaB - (areaS1 + areaS2)) > interval.err)
-    {
-		// assim, a área nova sera definida pela soma das áreas dos retangulos menores
-		// areaB = adaptativeQuadrature(interval.func, interval.a, m, interval.err) + adaptativeQuadrature(func, m, b, err);
-		leftInterval = rightInterval = interval;
-    	leftInterval.b = m;
-    	rightInterval.a = m;
-		askForThread(leftInterval);
-		threadIntervals[id] = rightInterval;
+    {        
+        leftInterval = rightInterval = interval;
+        leftInterval.b = m;
+        rightInterval.a = m;
+
+        threadIntervals[id].intervals[threadIntervals[id].size] = rightInterval;
+        if(!askForThread(leftInterval))
+        {
+            if(threadIntervals[id].size + 1 >= overloadMagicNumber)
+            {
+                threadIntervals[id].intervals[threadIntervals[id].size] = leftInterval;
+                storeOverloadedIntervals(threadIntervals[id]);
+            }
+        }
     }
     else
-    {    	
-    	goodToGo[id] = 0;
-    	imFree(id);
-    	pthread_mutex_lock(&sum_mutex);
-    	finalSum[id] += areaB;
-    	pthread_mutex_unlock(&sum_mutex);
-
-    	if(id == 0)
-    		end = 1;
+    {       
+        goodToGo[id] = 0;
+        imFree(id);        
+        finalSum[id] += areaB;
+        if (nFreeThreads == nThreads)
+        {
+            end = 1;
+        }
     }
 }
 
@@ -172,47 +223,61 @@ int main(int argc, char const *argv[])
     func = funcList[choice-'a'];
 
     pthread_mutex_init(&freeThreads_mutex, NULL);
-    pthread_mutex_init(&sum_mutex, NULL);
     pthread_cond_init(&freeThreads_cond, NULL);
-	
-	id = (int *) malloc(sizeof(int) * nThreads);
+    
+    id = (int *) malloc(sizeof(int) * nThreads);
     tid = (pthread_t *) malloc(sizeof(pthread_t) * nThreads);
     freeThreads = (int *) malloc(sizeof(int) * nThreads);
     goodToGo = (int *) malloc(sizeof(int) * nThreads);
-	threadIntervals = (Interval *) malloc(sizeof(Interval) * nThreads);
-	finalSum = (double *) calloc(sizeof(double), nThreads);
+    threadIntervals = (IntervalList *) malloc(sizeof(Interval) * nThreads);
+    finalSum = (double *) calloc(sizeof(double), nThreads);
+
+    for (int i = 0; i < nThreads; ++i)
+    {
+        threadIntervals[i].intervals = (Interval *) malloc(sizeof(Interval) * nThreads);
+        threadIntervals[i].size = 0;
+        threadIntervals[i].maxSize = overloadMagicNumber;
+    }
+
+    overloadIntervals.intervals = (Interval *) malloc(sizeof(Interval) * 10 * nThreads);
+    overloadIntervals.size = 0;
+    overloadIntervals.maxSize = 10*nThreads;
+
     //if(goodToGo == NULL) {printf("--ERRO: malloc()\n"); exit(1);}
-	//if(id == NULL) {printf("--ERRO: malloc()\n"); exit(1);}
+    //if(id == NULL) {printf("--ERRO: malloc()\n"); exit(1);}
     //if(tid == NULL) {printf("--ERRO: malloc()\n"); exit(1);}
     //if(freeThreads == NULL) {printf("--ERRO: malloc()\n"); exit(1);}
 
-	for (int i = 0; i < nThreads; ++i)
-	{		
-		id[i] = i;
-		goodToGo[i] = 1;
-    	freeThreads[i] = i;
-	}
-	end = 0;
-	for (int i = 0; i < nThreads; ++i)
-	{		
-		if (pthread_create(&tid[i], NULL, threadFunction, (void *) &id[i])) {
-      		printf("--ERRO: pthread_create()\n"); exit(-1);
-    	}
-	}	
-	askForThread((Interval){func, a, b, e});
+    for (int i = 0; i < nThreads; ++i)
+    {       
+        id[i] = i;
+        goodToGo[i] = 0;
+        freeThreads[i] = i;
+        nFreeThreads = nThreads;
+    }
+    end = 0;
+    for (int i = 0; i < nThreads; ++i)
+    {       
+        if (pthread_create(&tid[i], NULL, threadFunction, (void *) &id[i])) {
+            printf("--ERRO: pthread_create()\n"); exit(-1);
+        }
+    }   
+    askForThread((Interval){func, a, b, e});
 
-	while(!end);
+    for (int i = 0; i < nThreads; ++i)
+    {
+        pthread_join(tid[i], NULL);
+    }   
 
-	pthread_mutex_destroy(&sum_mutex);
-	pthread_mutex_destroy(&freeThreads_mutex);
- 	pthread_cond_destroy(&freeThreads_cond);
+    pthread_mutex_destroy(&freeThreads_mutex);
+    pthread_cond_destroy(&freeThreads_cond);
 
-	for (int i = 1; i < nThreads; ++i)
-	{
-		finalSum[0]+=finalSum[i];
-	}
+    for (int i = 1; i < nThreads; ++i)
+    {
+        finalSum[0]+=finalSum[i];
+    }
 
     printf("Approximate value for the integral of f from %lf to %lf: %lf\n", a, b, finalSum[0]);
 
-  	pthread_exit (NULL);
+    pthread_exit (NULL);
 }
